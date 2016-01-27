c5a49c23444168373e7591b343182850e4ea19df
dannysouza <danny.souza9@hotmail.com>
2016-01-27 17:38:26 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     int opt;
-    cout << "          Welcome          \n\n -- Chose an option -- ";
+    cout << "          Welcome          \n\n -- Choose an option -- ";
     Computer c1;
     do{
          cout << "\n\n 1 - Execute Game \n 2 - Execute Text Editor \n 0 - Turn Off \n";
@@ -21,7 +21,7 @@ int main(){
                      cout << "\n\n Turning off... ";
                      break;
                 default:
-                        cout << "\n\n -- Chose an option -- ";
+                        cout << "\n\n -- Choose an option -- ";
          }
     }while (opt != 0);         
 }    
